c5e268c689988dbd8f0b032f54ff8b5677330fad
Jonathan Johnson <95863021+johnnyd81@users.noreply.github.com>
2023-12-17 19:40:55 +0200

    Update Error.jsx

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,4 +1,5 @@
 const Error = () => {
+  //the error component is rendered when an error occurs while loading the questions
   return (
     <p className="error">
       <span className="emoji">😢</span> An error occurred while loading the
